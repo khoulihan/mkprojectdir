515976db492ee0ee5229f491e877c897c7797039
Kevin Houlihan <kevin@crimsoncookie.com>
2020-11-15 18:18:38 +0100

    Changed project 'type' to 'template'. Added README text.

---
+++ README.md
@@ -0,0 +1,19 @@
+# mkprojectdir
+
+A script to create standard directory structures for projects.
+
+Copyright (c) 2020 by Kevin Houlihan
+
+License: MIT, see LICENSE for more details.
+
+## Prerequisites
+
+The script depends on Python 3.7 (though possibly earlier versions of Python 3 will work fine).
+
+## Usage
+
+The script takes two arguments - a project template and a destination. The template is copied to the destination.
+
+### Project Template
+
+The project template can be a path to a directory, or the name of a template in the `project_templates` directory in the script's config directory (`~/mkprojectdir` by default).
--- mkprojectdir.py
+++ mkprojectdir.py
@@ -12,14 +12,14 @@ _debug = False
 
 def _parse_arguments():
     parser = argparse.ArgumentParser(description="Create standard directory structures for projects.")
-    parser.add_argument("project_type", type=str, help="the type of project to create")
+    parser.add_argument("project_template", type=str, help="the template to use for the project")
     parser.add_argument("destination", type=str, action="store", help="directory to create for the project")
     parser.add_argument("-d", "--debug", action="store_true", dest="debug", help="print debugging information")
     args = parser.parse_args()
     return args
 
 
-def _get_project_type_path(pt):
+def _get_project_template_path(pt):
     pt_path = Path(pt)
     if pt_path.exists():
         return pt_path
@@ -27,7 +27,7 @@ def _get_project_type_path(pt):
         xdg_config = Path(os.environ['XDG_CONFIG_HOME'])
     except KeyError:
         xdg_config = Path('~/.config').expanduser()
-    config_path = xdg_config / 'mkprojectdir' / 'project_types'
+    config_path = xdg_config / 'mkprojectdir' / 'project_templates'
     if not config_path.exists():
         config_path.mkdir(parents=True)
     pt_path = config_path / pt
@@ -51,9 +51,9 @@ def _main():
     _debug = args.debug
     try:
         try:
-            pt_path = _get_project_type_path(args.project_type)
+            pt_path = _get_project_template_path(args.project_template)
         except FileNotFoundError as e:
-            print ("The specified project type does not exist")
+            print ("The specified project template does not exist")
             sys.exit(1)
 
         try:
