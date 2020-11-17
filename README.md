81a22d005283517ab60ef825502dccdd8e5c7507
Kevin Houlihan <kevin@crimsoncookie.com>
2020-11-17 10:50:45 +0100

    Switched to command based UI with commands for managing templates

--- README.md
+++ README.md
@@ -12,14 +12,40 @@ The script depends on Python 3.7 (though possibly earlier versions of Python 3 w
 
 ## Usage
 
-The script takes two arguments - a project template and a destination. The template is copied to the destination.
+The script accepts three commands - create, list, and save.
 
-### Project Template
+### List
 
-The project template can be a path to a directory, or the name of a template in the `project_templates` directory in the script's config directory (`~/.config/mkprojectdir` by default).
+This command accepts no arguments, and just lists the available project templates.
+
+`ls` is another alias for this command.
+
+```
+mkprojectdir list
+```
+
+### Save
+
+Saves a specified directory as a template. By default the template will have the same name as the directory, but a different name can be specified with the `--name` (`-n` for short) switch.
+
+```
+mkprojectdir save --name art ./art_template
+```
+
+### Create
+
+Creates a project directory based on a template. This command accepts two arguments - a project template to use, and a destination path.
+
+The project template can be a path to a directory, or the name of a template in the `project_templates` directory in the script's config directory (`~/.config/mkprojectdir` by default). Templates can be saved to this directory using the `save` command described above.
+
+```
+mkprojectdir create art ./new_art_project
+```
+
+## Project Templates
 
 A template is just a directory structure with whatever files are required for the project type in question.
 
-File and directory names in a template can include variables wrapped in braces (e.g. `{Project Name}.md`). In this event, values will be requested interactively and these variables will be replaced in the destination.
+File and directory names in a template can include variables wrapped in braces (e.g. `{Project Name}.md`). In this event, values will be requested interactively when the template is used, and these variables will be replaced in the destination.
 
 Text files of several types will also be searched for variables to replace. Variables should be wrapped in double braces (`{{...}}`) for markdown, plain-text, HTML, TOML and INI files, and double angle brackets (`<<...>>`) for JSON, YAML and Python files. Other file types are not searched at this time.
--- mkprojectdir.py
+++ mkprojectdir.py
@@ -6,6 +6,7 @@ import argparse
 from pathlib import Path
 import shutil
 import re
+import tempfile
 
 
 _debug = False
@@ -19,22 +20,47 @@ _double_angles_suffixes = ['.json', '.yaml', '.py']
 
 def _parse_arguments():
     parser = argparse.ArgumentParser(description="Create standard directory structures for projects.")
-    parser.add_argument("project_template", type=str, help="the template to use for the project")
-    parser.add_argument("destination", type=str, action="store", help="directory to create for the project")
     parser.add_argument("-d", "--debug", action="store_true", dest="debug", help="print debugging information")
+    subparsers = parser.add_subparsers(dest='command')
+
+    # Create command
+    create_parser = subparsers.add_parser('create', aliases=['c', 'mk'], help="create a project directory")
+    create_parser.add_argument("project_template", type=str, help="the template to use for the project")
+    create_parser.add_argument("destination", type=str, action="store", help="directory to create for the project")
+
+    # List templates command
+    list_parser = subparsers.add_parser('list', aliases=['ls'], help="list available templates")
+
+    # Save template command
+    save_parser = subparsers.add_parser('save', aliases=['s'], help="save a directory as a template")
+    save_parser.add_argument("template", type=str, action="store", help="directory to save as a template")
+    save_parser.add_argument("-n", "--name", type=str, action="store", dest="name", help="name to use for the template, if it should be different from the original directory name")
+
     args = parser.parse_args()
     return args
+
+
+def _get_template_directory():
+    try:
+        xdg_config = Path(os.environ['XDG_CONFIG_HOME'])
+    except KeyError:
+        xdg_config = Path('~/.config').expanduser()
+    return xdg_config / 'mkprojectdir' / 'project_templates'
+
+
+def _get_template_backup_directory():
+    try:
+        xdg_config = Path(os.environ['XDG_CONFIG_HOME'])
+    except KeyError:
+        xdg_config = Path('~/.config').expanduser()
+    return xdg_config / 'mkprojectdir' / 'template_backups'
 
 
 def _get_project_template_path(pt):
     pt_path = Path(pt)
     if pt_path.exists():
         return pt_path
-    try:
-        xdg_config = Path(os.environ['XDG_CONFIG_HOME'])
-    except KeyError:
-        xdg_config = Path('~/.config').expanduser()
-    config_path = xdg_config / 'mkprojectdir' / 'project_templates'
+    config_path = _get_template_directory()
     if not config_path.exists():
         config_path.mkdir(parents=True)
     pt_path = config_path / pt
@@ -142,41 +168,88 @@ def _replace_variables(destination, replacements):
             f.rename(f.parent / name)
 
 
+def _create(args):
+    try:
+        pt_path = _get_project_template_path(args.project_template)
+    except FileNotFoundError as e:
+        print ("The specified project template does not exist")
+        sys.exit(1)
+
+    try:
+        destination = _verify_destination(args.destination)
+    except FileExistsError:
+        print ("The specified destination already exists.")
+        sys.exit(1)
+    except FileNotFoundError:
+        print ("The specified destination directory could not be created because of missing parents.")
+        sys.exit(1)
+    except PermissionError:
+        print ("The destination directory could not be created due to inadequate permissions.")
+        sys.exit(1)
+
+    for content in pt_path.iterdir():
+        if content.is_file():
+            shutil.copy(content, destination)
+        else:
+            shutil.copytree(content, destination / content.name, copy_function=shutil.copy)
+
+    variables = _find_variables(destination)
+    if variables:
+        print ("The specified template included variables for replacement.")
+        print ("Please enter the desired values.")
+        replacements = _get_replacements(variables)
+        _replace_variables(destination, replacements)
+
+
+def _list_templates(args):
+    config_path = _get_template_directory()
+    if not config_path.exists():
+        config_path.mkdir(parents=True)
+    print("Available templates:")
+    print()
+    for template in config_path.iterdir():
+        print(template.name)
+
+
+def _save_template(args):
+    config_path = _get_template_directory()
+    if not config_path.exists():
+        config_path.mkdir(parents=True)
+
+    template = Path(args.template)
+    if not template.exists():
+        print("The specified template does not exist.")
+        sys.exit(1)
+    if not template.is_dir():
+        print("Only directories can be saved as templates.")
+        sys.exit(1)
+    name = template.name
+    if args.name:
+        name = args.name
+    saved_path = config_path / name
+    if saved_path.exists():
+        backup_directory = _get_template_backup_directory()
+        if not backup_directory.exists():
+            backup_directory.mkdir(parents=True)
+        template_backup = backup_directory / name
+        if template_backup.exists():
+            shutil.rmtree(template_backup)
+        shutil.copytree(saved_path, template_backup, copy_function=shutil.copy)
+        shutil.rmtree(saved_path)
+    shutil.copytree(template, saved_path, copy_function=shutil.copy)
+
+
 def _main():
     args = _parse_arguments()
     global _debug
     _debug = args.debug
     try:
-        try:
-            pt_path = _get_project_template_path(args.project_template)
-        except FileNotFoundError as e:
-            print ("The specified project template does not exist")
-            sys.exit(1)
-
-        try:
-            destination = _verify_destination(args.destination)
-        except FileExistsError:
-            print ("The specified destination already exists.")
-            sys.exit(1)
-        except FileNotFoundError:
-            print ("The specified destination directory could not be created because of missing parents.")
-            sys.exit(1)
-        except PermissionError:
-            print ("The destination directory could not be created due to inadequate permissions.")
-            sys.exit(1)
-
-        for content in pt_path.iterdir():
-            if content.is_file():
-                shutil.copy(content, destination)
-            else:
-                shutil.copytree(content, destination / content.name, copy_function=shutil.copy)
-
-        variables = _find_variables(destination)
-        if variables:
-            print ("The specified template included variables for replacement.")
-            print ("Please enter the desired values.")
-            replacements = _get_replacements(variables)
-            _replace_variables(destination, replacements)
+        if args.command in ["create", "c"]:
+            _create(args)
+        elif args.command in ["list", "ls"]:
+            _list_templates(args)
+        elif args.command in ["save", "s"]:
+            _save_template(args)
     except KeyboardInterrupt:
         print()
         sys.exit(0)
